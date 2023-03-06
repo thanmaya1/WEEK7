87f86cc94fde3099c632b21a7d711dc282ef687a
Thanmaya Garimella <112673461+thanmaya1@users.noreply.github.com>
2023-03-06 07:42:56 +0530

    Create 117.c

---
+++ 117.c
@@ -0,0 +1,9 @@
+push('P');  // push 'P' into the stack
+push('U');  // push 'U' into the stack
+pop();      // pop the top element from the stack ('U' is removed)
+push('S');  // push 'S' into the stack
+push('H');  // push 'H' into the stack
+pop();      // pop the top element from the stack ('H' is removed)
+push('P');  // push 'P' into the stack
+pop();      // pop the top element from the stack ('P' is removed)
+push('A');  // push 'A' into the stack
